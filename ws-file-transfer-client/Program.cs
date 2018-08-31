0bffc7056e86961e9ce01fbda1ef5d6f384627ff
Luiz Filipe Machado Barni <machado@odahcam.com>
2018-08-30 21:39:29 -0300

    first commit

---
+++ ws-file-transfer-client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ws_file_transfer_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<<< Async TCP Client >>>");
+
+            Console.Write("IP do servidor: ");
+
+            var ipAddress = IPAddress.Parse(Console.ReadLine());
+
+            var server = new IPEndPoint(ipAddress, 11000);
+
+            var client = new TcpClient();
+
+            client.Connect(server);
+
+            Console.WriteLine($"Conectado a {client.Client.RemoteEndPoint}.");
+
+            Console.WriteLine("Preparando arquivo para envio...");
+
+            var fileStream = File.OpenRead("FileToSend.txt");
+
+            var fileBuffer = new byte[fileStream.Length];
+
+            Console.WriteLine("Lendo arquivo...");
+
+            fileStream.Read(fileBuffer, 0, (int)fileStream.Length);
+
+            var networkStream = client.GetStream();
+
+            Console.WriteLine("Enviando arquivo...");
+
+            networkStream.Write(fileBuffer, 0, (int)fileStream.Length);
+
+            networkStream.Close();
+            fileStream.Close();
+
+            Console.WriteLine("Arquivo enviado.");
+
+            client.Close();
+
+            Console.WriteLine("Conexão encerrada.");
+        }
+    }
+}
---
+++ ws-file-transfer-server/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ws_file_transfer_server
+{
+    class Program
+    {
+        static int fileCounter = 0;
+
+        static void Main(string[] args)
+        {
+            StartListening();
+
+            Console.WriteLine("Pressione ENTER para finalizar.");
+            Console.Read();
+        }
+
+        static void StartListening()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("<<< Async TPC Listener >>>");
+                Console.WriteLine();
+
+                IPEndPoint server = null;
+
+                var host = Dns.GetHostEntry(Dns.GetHostName());
+
+                foreach (var ip in host.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        server = new IPEndPoint(ip, 11000);
+                    }
+                }
+
+                var listener = new TcpListener(server);
+
+                listener.Start();
+
+                Console.WriteLine($"Servidor {listener.LocalEndpoint}...");
+
+                WaitForClients(listener);
+
+                Console.WriteLine("Aguardando transferência de arquivos...");
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+                throw;
+            }
+
+            return;
+        }
+
+        private static void WaitForClients(TcpListener listener)
+        {
+            try
+            {
+                listener.BeginAcceptTcpClient(new AsyncCallback(OnClientConnect), listener);
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        private static void OnClientConnect(IAsyncResult ar)
+        {
+            try
+            {
+                var listener = ar.AsyncState as TcpListener;
+                var handler = listener.EndAcceptTcpClient(ar);
+
+                Console.WriteLine();
+                Console.WriteLine($"Connectado ao cliente {handler.Client.RemoteEndPoint}...");
+
+                WaitForClients(listener);
+
+                Console.WriteLine("Recebendo arquivo...");
+
+                var networkStream = new NetworkStream(handler.Client);
+
+                int bufferSize = 1024;
+
+                var buffer = new byte[bufferSize];
+
+                string fileName = $"ReceivedFile-{fileCounter}.txt";
+
+                fileCounter++;
+
+                var fileStream = File.OpenWrite(fileName);
+
+                int bytesRead = -1;
+
+                while (bytesRead != 0)
+                {
+                    bytesRead = networkStream.Read(buffer, 0, bufferSize);
+                    fileStream.Write(buffer, 0, bytesRead);
+                }
+
+                fileStream.Close();
+                networkStream.Close();
+
+                Console.WriteLine("Arquivo recebido.");
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
